3931f83843b442406166ceaa9c241abed3b27264
MaksonViini <maksonvinicio7@gmail.com>
2021-03-02 13:03:45 -0300

    Montando a estrutura do repositorio para o novo projeto de risco de credito

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Project-Credit-Risc
 inicializando
+
+### Link dos arquivos
+
+[link](https://www.kaggle.com/caesarlupum/risco-de-credito?select=teste.csv)
---
+++ app.py
@@ -0,0 +1,14 @@
+# Aqui vai ter o APP
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+print('Deploy ainda a ser feito')
+
+# título
+st.title("Sistema de Previsão de Risco de Credito- By Makson Vinicio")
+
+# subtítulo
+st.markdown("Este é um Aplicativo utilizado para exibir a solução de Ciência de Dados para o problema de predição de Risco de credito.")
+
+st.sidebar.subheader("Credito")
